c562748a5345a62f63ff027e13565a6017e92d85
Lelouch <beimengtianxia@gmail.com>
2023-03-27 15:38:37 +0800

    修改一些版本限制，及node:module 前缀模式

--- src/course/test.js
+++ src/course/test.js
@@ -1,6 +1,2 @@
-import fs from 'fs/promises'
-
 const text = '测试内容'
-console.log(fs)
-
 export { text }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import { text } from './course/test.js'
 
+import { open, readdir } from 'node:fs/promises'
+import path, { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 console.log(text)
-console.log(123123)
+
+// const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+let file
+try {
+  file = await open(resolve(__dirname, 'assets/plate.csv'))
+
+  for await (const line of file.readLines()) {
+    console.log(line)
+  }
+} finally {
+  await file?.close()
+}
+
+try {
+  const files = await readdir('./')
+  for (const file of files) console.log(file)
+} catch (err) {
+  console.error(err)
+}
